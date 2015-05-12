cb1b318efe995a9983b97e13a7267b8991633cd5
Trejkaz (pen name) <trejkaz@trypticon.org>
2015-05-12 14:19:13 +1000

    Bit of a sanity check for the test.

--- src/test/java/org/trypticon/luceneupgrader/Test.java
+++ src/test/java/org/trypticon/luceneupgrader/Test.java
@@ -1,11 +1,8 @@
 package org.trypticon.luceneupgrader;
 
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.IndexInput;
-import org.apache.lucene.store.IndexOutput;
 import org.trypticon.luceneupgrader.lucene3.IndexUpgrader3;
+import org.trypticon.luceneupgrader.lucene3.internal.lucene.index.IndexReader;
+import org.trypticon.luceneupgrader.lucene3.internal.lucene.store.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,7 +10,7 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws IOException {
         try (Directory directory = FSDirectory.open(new File("/Data/Lucene/TextIndexCopy"))) {
-            // copy some test data in
+            // copy an old v2 index
             for (String name : directory.listAll()) {
                 directory.deleteFile(name);
             }
@@ -29,7 +26,27 @@ public class Test {
             new IndexUpgrader3(directory).upgrade();
 
             try (IndexReader reader = IndexReader.open(directory)) {
-                System.out.println(reader.numDocs());
+                System.out.println("doc count after: " + reader.numDocs());
+            }
+
+            try (Directory expectedDirectory = FSDirectory.open(new File("/Data/Lucene/TextIndexReferenceResult"))) {
+                for (String name : expectedDirectory.listAll()) {
+                    try (IndexInput input = directory.openInput(name);
+                         IndexInput expectedInput = expectedDirectory.openInput(name)) {
+                        long length = expectedInput.length();
+                        if (input.length() != length) {
+                            throw new AssertionError();
+                        }
+
+                        while (input.getFilePointer() < length) {
+                            byte expected = expectedInput.readByte();
+                            byte actual = input.readByte();
+                            if (actual != expected) {
+                                throw new AssertionError();
+                            }
+                        }
+                    }
+                }
             }
         }
     }
