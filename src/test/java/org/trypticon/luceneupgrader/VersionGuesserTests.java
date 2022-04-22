8f12249497a46067c47f6c480cd38fbf3914a487
Hakanai <hakanai@ephemeral.garden>
2022-04-22 11:28:59 +1000

    Fixing deprecation warning on old assertThat

--- src/test/java/org/trypticon/luceneupgrader/VersionGuesserTests.java
+++ src/test/java/org/trypticon/luceneupgrader/VersionGuesserTests.java
@@ -13,7 +13,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Tests for {@link VersionGuesser}.
